b10f340c25504e7529eb05d3dc7f3ef4beaf6c02
LAPTOP-PPOG0DGB <ghfc101@hotmail.com>
2022-02-25 22:55:52 +0000

    Added carousel to home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
 
 export class Home extends React.Component {
     render() {
         return(
-            <div>
-                <h2>Home</h2>
-            </div>
+            <Carousel>
+                <Carousel.Item>
+                    <img className="d-block w-100 carousel-pic" src="/category.jpg" alt="First slide" />
+                    <Carousel.Caption>
+                    <h3>Horse Category</h3>
+                    <p>To be edited</p>
+                    <p><a class="btn btn-large btn-outline-light" href="#">Browse by Category</a></p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className="d-block w-100 carousel-pic" src="/discipline.jpg" alt="Second slide" />
+                    <Carousel.Caption>
+                    <h3>Horse Discipline</h3>
+                    <p>To be edited</p>
+                    <p><a class="btn btn-large btn-outline-light" href="#">Browse by Discipline</a></p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className="d-block w-100 carousel-pic" src="/weight.jpg" alt="Third slide" />
+                    <Carousel.Caption>
+                    <h3>Horse Size</h3>
+                    <p>To be edited</p>
+                    <p><a class="btn btn-large btn-outline-light" href="#">Browse by Size</a></p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         )
     }    
 }
