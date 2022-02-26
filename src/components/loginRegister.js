36f32a6b081f611661fb352697f3d7ac6dcb84d0
LAPTOP-PPOG0DGB <ghfc101@hotmail.com>
2022-02-26 20:29:25 +0000

    Added forms in login/register page

--- src/components/loginRegister.js
+++ src/components/loginRegister.js
@@ -1,11 +1,66 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 export class LoginRegister extends React.Component {
     render() {
-        return(
-            <div>
-                <h2>Login/Register</h2>
-            </div>
+        return (
+            <Container className='login-container'>
+                <Row>
+                    <Col>
+                        <div>
+                            <form>
+                                <h3>Login</h3>
+
+                                <div className="form-group">
+                                    <label>Email</label>
+                                    <input type="email" className="form-control" placeholder="Enter email" />
+                                </div>
+
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <input type="password" className="form-control" placeholder="Enter password" />
+                                </div>
+
+                                <div className="form-group">
+                                    <div className="custom-control custom-checkbox">
+                                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                                    </div>
+                                </div>
+
+                                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+                            </form>
+                        </div>
+                    </Col>
+                    <Col>
+                        <form>
+                            <h3>Register</h3>
+
+                            <div className="form-group">
+                                <label>First name</label>
+                                <input type="text" className="form-control" placeholder="First name" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Last name</label>
+                                <input type="text" className="form-control" placeholder="Last name" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input type="email" className="form-control" placeholder="Enter email" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input type="password" className="form-control" placeholder="Enter password" />
+                            </div>
+
+                            <button type="submit" className="btn btn-lg btn-dark btn-block">Register</button>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
         )
-    }    
+    }
 }
