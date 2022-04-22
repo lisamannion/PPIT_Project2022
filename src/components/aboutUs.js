b51d14c46cd2ff32350d6fadcdffa6f037dc564f
LAPTOP-PPOG0DGB <ghfc101@hotmail.com>
2022-04-22 21:15:29 +0100

    Added code to construct about us page

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 export class AboutUs extends React.Component {
     render() {
-        return(
-            <div>
-                <h2>About Us</h2>
+        return (
+            <div className="about-wrap">
+                <div className="about-inner">
+                    <img src="about.jpg" alt="backgound-image" />
+                    <div className="about-caption text-center">
+                        <h1>About Us</h1>
+                        <h3>
+                            Description here
+                        </h3>
+                    </div>
+                </div>
             </div>
         )
-    }    
+    }
 }
