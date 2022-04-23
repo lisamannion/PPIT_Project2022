f979a394c5973610bdece52775f1f23fb0aa6d39
LAPTOP-PPOG0DGB <ghfc101@hotmail.com>
2022-04-23 09:38:54 +0100

    Added routing to carousel in home page

--- src/components/home.js
+++ src/components/home.js
@@ -9,25 +9,25 @@ export class Home extends React.Component {
                 <Carousel.Item>
                     <img className="d-block w-100 carousel-pic" src="/category.jpg" alt="First slide" />
                     <Carousel.Caption>
-                        <h3>Horse Category</h3>
-                        <p>To be edited</p>
-                        <p><a className="btn btn-large btn-outline-light" href="#">Browse by Category</a></p>
+                        <h3>Been wanting to own a horse?</h3>
+                        <p>Take a look at the horses we have!</p>
+                        <p><a className="btn btn-large btn-outline-light" href="/listings">Click Here</a></p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img className="d-block w-100 carousel-pic" src="/discipline.jpg" alt="Second slide" />
                     <Carousel.Caption>
-                        <h3>Horse Discipline</h3>
-                        <p>To be edited</p>
-                        <p><a className="btn btn-large btn-outline-light" href="#">Browse by Discipline</a></p>
+                        <h3>Wish to advertise your horse(s)?</h3>
+                        <p>Create an ad for your horse!</p>
+                        <p><a className="btn btn-large btn-outline-light" href="/createAd">Create Now</a></p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img className="d-block w-100 carousel-pic" src="/weight.jpg" alt="Third slide" />
                     <Carousel.Caption>
-                        <h3>Horse Size</h3>
-                        <p>To be edited</p>
-                        <p><a className="btn btn-large btn-outline-light" href="#">Browse by Size</a></p>
+                        <h3>About Us</h3>
+                        <p>Want to know more about what we offer and what we do?</p>
+                        <p><a className="btn btn-large btn-outline-light" href="/aboutUs">Know More</a></p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
